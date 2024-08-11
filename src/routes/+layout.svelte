0f4b9ad65328a58214981b60f455244024d6d0f0
Chris I Powell <chris@chrisipowell.co.uk>
2024-08-11 17:41:53 +0100

    Add layout with basic header and global font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<style>
+    :global(body) {
+        font-family: 'Ink Free';
+    }
+</style>
+
+<header>
+    <h1>Chris I Powell</h1>
+</header>
+
+<slot></slot>
+
+<footer>CIPowell 2024</footer>
