0bac6b6530c6e3643b7ba045a506e5a272ff62df
Chris Powell <chris@chrisipowell.co.uk>
2024-09-08 08:58:28 +0100

    Setup unit tests with svelte testing library (#4)

---
+++ src/lib/Header.test.ts
@@ -0,0 +1,7 @@
+import { test } from "vitest";
+import { render } from "@testing-library/svelte";
+import Header from "./Header.svelte";
+
+test("Renders the header successfully", () => {
+    render(Header);
+});
