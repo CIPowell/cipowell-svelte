495a1c74110b73ff92b5b6d0213f0f8c05bccfc3
Chris Powell <chris@chrisipowell.co.uk>
2024-09-14 09:40:46 +0100

    Add Storybook (resolves #6) (#8)
    * Add Storybook and move CSS file to allow it in preview (#6)
    
    * add first stories

---
+++ src/lib/molecules/nav/Nav.stories.svelte
@@ -0,0 +1,37 @@
+<script context="module" lang="ts">
+    export const meta = {
+        title: "Design System/Molecules/ Navigation",
+        component: Nav
+    }
+</script>
+
+<script lang="ts">
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+    import Nav from "./Nav.svelte";
+	import { userEvent, within } from "@storybook/test";
+
+    function sleep(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+</script>
+
+<Template let:args>
+    <Nav {...args} />
+</Template>
+
+<Story name="Demo Bar" 
+    args={{links:[{title: 'Home', target: '/'},{title: 'Blog', target: '/blog'}]}} 
+    play={async ({canvasElement}) => {
+        const canvas = within(canvasElement);
+        await sleep(1000);
+
+        const homeLink = await canvas.findByText('Home');
+        const blogLink = await canvas.findByText('Blog');
+
+        userEvent.click(homeLink);
+        await sleep(2000);
+
+        userEvent.click(blogLink);
+        await sleep(2000);
+    }}
+/>
