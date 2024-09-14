839579844b913f496324bd69e319588d3f819a73
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-14 21:26:38 +0100

    Improve error handling

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,24 @@
 import { NavigationService, type NavLink } from "$lib/services/navigation/nav";
+import { error } from "@sveltejs/kit";
+import Layout from "./+layout.svelte";
+
+interface LayoutData {
+    navLinks: NavLink[]
+}
 
 export async function load() {
     const navService = new NavigationService();
+    const layoutData: LayoutData = {
+        navLinks: []
+    };
 
-    return {
-        navLinks : await navService.getGlobalNavLinks()
+    try {
+        layoutData.navLinks = await navService.getGlobalNavLinks();
+    } catch(err) {
+        error(500, {
+            message: 'Failed to load Layout data'
+        });
     }
+
+    return layoutData;
 }
