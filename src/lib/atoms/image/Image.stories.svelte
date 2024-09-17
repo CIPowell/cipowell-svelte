91cc82b721d91ab2199707f67d5d87808f6da613
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-17 23:37:07 +0100

    Add simple image element

---
+++ src/lib/atoms/image/Image.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+    export const meta = {
+        title: "Design System/atoms/Image",
+        component: Image
+    }
+</script>
+
+<script lang="ts">
+    import { Template, Story } from "@storybook/addon-svelte-csf";
+    import Image from "./Image.svelte";
+</script>
+
+<Template let:args>
+    <Image {...args} />
+</Template>
+
+<Story name="Placeholder" args={{
+    image: "https://placehold.co/600x400",
+    altText: "Placeholder"
+}} />
