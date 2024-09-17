2f61374870f66b67e5580d883483e70e45c0d861
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-17 23:31:28 +0100

    Use pages to render all the routes

---
+++ src/routes/[...catchall]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { isPreviewing, Content } from "@builder.io/sdk-svelte";
+
+    export let data;
+    export let api_key;
+    export let model = 'page'
+
+    const canShowData = data.content || isPreviewing();
+</script>
+
+<main>
+    {#if canShowData}
+        <Content
+            model={model}
+            content={data.content}
+            apiKey={api_key} 
+        />
+    {:else}
+        Content not found
+    {/if}
+</main>
