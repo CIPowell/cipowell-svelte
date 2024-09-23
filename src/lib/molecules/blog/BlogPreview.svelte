a8ad48d537f555eb5e5cecd6182510ac6225a5da
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-23 23:00:10 +0100

    Add a blog preview

---
+++ src/lib/molecules/blog/BlogPreview.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    export let title: string;
+    export let slug: string;
+    export let previewText: string;
+    export let imageSrc: string; 
+    export let imageAltText: string;
+
+    import Image from "$lib/atoms/image/Image.svelte";
+</script>
+
+<style>
+    article {
+        width: 100%;
+    }
+
+    a::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+
+    a {
+        color: #000;
+        display: block;
+        text-decoration: none;
+    }
+
+    a:hover {
+        background-color: #ddd;
+
+        transition: 200ms ease-in-out;
+
+        margin: -1em;
+        padding: 1em;
+    }
+
+    p {
+        line-height: 1.3rem;
+        font-size: 1rem;
+    }
+</style>
+
+<article>
+    <a href="/blog{slug}">
+        <h1>{title}</h1>
+        <Image image={imageSrc} altText={imageAltText} width={250} left={true} />
+        <p>{previewText}</p>
+    </a>
+</article>
