599af1a1a27c4e27f5008c434aa4bb02cd6879c8
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-23 21:48:31 +0100

    Add left floating image so for blog preview

--- src/lib/atoms/image/Image.svelte
+++ src/lib/atoms/image/Image.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
-    export let image: string;
-    export let altText: string;
+	export let image: string;
+	export let altText: string;
+	export let width: number;
+	export let left: boolean = false;
 </script>
 
-<img src={image} alt={altText} />
+<picture class:left>
+	<source srcSet={`${image}?width=${width}&format=avif`} />
+	<source srcSet={`${image}?width=${width}&format=webp`} />
+	<img src={`${image}?width=${width}&format=png`} alt={altText} {width} />
+</picture>
+
+<style>
+	picture {
+		max-width: 100%;
+		margin: 0 1rem 0.5rem 0;
+	}
+
+	.left {
+		display: block;
+		float: left;
+	}
+</style>
