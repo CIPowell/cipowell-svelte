d1c957103a6ac36d959572893ead2b67d54618f3
Chris I Powell <chris@chrisipowell.co.uk>
2024-09-23 23:00:10 +0100

    Add a blog preview

---
+++ src/lib/molecules/blog/BlogPreview.stories.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	export const meta = {
+		title: 'Design System/molecules/blog/Blog Preview',
+		component: BlogPreview
+	};
+</script>
+
+<script lang="ts">
+	import { Template, Story } from '@storybook/addon-svelte-csf';
+	import BlogPreview from './BlogPreview.svelte';
+</script>
+
+<Template let:args>
+	<BlogPreview {...args} />
+</Template>
+
+<Story
+	name="Normal"
+	args={{
+		title: 'Blog title',
+		slug: '/article',
+		previewText:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo in ligula a euismod. Aliquam tincidunt sem tincidunt ante condimentum semper. Morbi cursus justo urna, in finibus dui pulvinar in. Curabitur commodo dolor vitae nisi hendrerit, faucibus aliquam elit gravida. Phasellus lorem ipsum, sodales nec nulla id, posuere iaculis quam. Cras feugiat, dolor a lacinia tincidunt, est quam pellentesque ante, id condimentum elit ante quis massa. Integer sodales ante sit amet metus tristique, ac sagittis arcu efficitur. Nam tincidunt, odio sit amet venenatis tempor, tortor velit mollis leo, a finibus tellus mauris a dolor. Nam id nisi felis. Maecenas cursus sem eget est consectetur eleifend. Pellentesque id ornare urna. Proin blandit leo nec velit pulvinar, non volutpat enim sodales. Sed vel sagittis nunc, id dignissim velit.',
+		imageSrc: 'https://placehold.co/600x400',
+		imageAltText: 'placeholder Image'
+	}}
+/>
