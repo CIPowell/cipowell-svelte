f6e96cb5e5213cd5607779c848907fe87ecbf604
Chris I Powell <chris@chrisipowell.co.uk>
2025-05-15 22:25:02 +0100

    Load and Render pages from Contentful

--- src/lib/services/cms/contentful.ts
+++ src/lib/services/cms/contentful.ts
@@ -1,10 +1,12 @@
 import { env } from '$env/dynamic/private';
 import type { NavClient, NavLink } from '$lib/services/navigation/nav';
 import * as contentful from 'contentful';
-import type { Page } from './content_types';
+import type { ContentfulPage } from './content_types';
+import type { Page, PageClient } from '$lib/services/page/Page';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import { page } from '$app/state';
 
-export class Contentful implements NavClient {
+export class Contentful implements NavClient, PageClient {
     client: contentful.ContentfulClientApi<undefined>;
 
     constructor() {
@@ -16,7 +18,7 @@ export class Contentful implements NavClient {
     }
 
     async getGlobalNavLinks(): Promise<NavLink[]> {
-        let pages = await this.client.getEntries<Page>({
+        let pages = await this.client.getEntries<ContentfulPage>({
             'content_type': 'page',
         });
 
@@ -25,6 +27,26 @@ export class Contentful implements NavClient {
                 title: p.fields.title,
                 target: p.fields.slug
             }));
+    }
+
+    async getPage(slug: string): Promise<Page> {
+        let pages = await this.client.getEntries<ContentfulPage>({
+            'content_type': 'page',
+            'fields.slug': slug
+        });
+
+        if (pages.items.length === 0) {
+            throw new Error(`Page not found: ${slug}`);
+        }
+
+        let page = pages.items[0];
+
+        return {
+            title: page.fields.title,
+            slug: page.fields.slug,
+            content: documentToHtmlString(page.fields.content),
+            breadcrumbs: []
+        };
     }
 };
 
--- src/routes/[...catchall]/+page.svelte
+++ src/routes/[...catchall]/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-    
-
+    let { data } = $props();
 </script>
 
+<svelte:head>
+    <title>Chris I Powell - {data.title}</title>
+</svelte:head>
+
 <main>
-   
+   <h1>{data.title}</h1>
+   {@html data.content}
 </main>
