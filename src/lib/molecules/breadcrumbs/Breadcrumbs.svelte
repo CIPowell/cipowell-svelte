4b1ab87d75a551f2f4a702b6092654c7d80f85b5
Chris I Powell <chris@chrisipowell.co.uk>
2025-05-17 20:14:33 +0100

    Add in Breadcrumbs

---
+++ src/lib/molecules/breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { NavLink } from '$lib/services/navigation/nav';
+	export let links: NavLink[] = [];
+</script>
+
+<nav class="breadcrumbs">
+	{#each links as link, i}
+		<a href={link.target}>{link.title}</a>
+		{#if i < links.length - 1}
+			<span class="sep">&gt;</span>
+		{/if}
+	{/each}
+</nav>
+
+<style>
+	.breadcrumbs {
+		padding: 0.5em 0;
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+	}
+
+	.breadcrumbs .sep {
+		padding: 0 0.5em;
+		color: #888;
+	}
+  
+	.breadcrumbs a {
+		text-decoration: none;
+	}
+</style>
