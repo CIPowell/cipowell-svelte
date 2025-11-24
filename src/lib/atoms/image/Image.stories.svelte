dca49d10df46f6eaef6fd875b5d744cffe077609
Chris I Powell <chris@chrisipowell.co.uk>
2025-11-24 20:21:16 +0000

    Move to storybook svelte csf and add in the vitest addin

---
+++ src/lib/atoms/image/Image.stories.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Image from './Image.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Design System/atoms/Image',
+		component: Image
+	});
+</script>
+
+<Story name="Placeholder" args={{ image: 'https://placehold.co/600x400', altText: 'Placeholder', width: 600 }} />
+
+<Story
+	name="ArticlePreview"
+	args={{
+		image: 'https://placehold.co/600x400',
+		altText: 'Placeholder',
+		width: 250,
+		left: true
+	}}
+/>
--- src/lib/atoms/taglist/TagList.stories.svelte
+++ src/lib/atoms/taglist/TagList.stories.svelte
@@ -1,33 +1,30 @@
-<script module lang="ts">
-    export const meta = {
-        title: "Design System/Atoms/taglist/TagList",
-        component: TagList
-    }
+<script context="module">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn, userEvent, within, waitFor, expect } from 'storybook/test';
+	import TagList from './TagList.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Design System/Atoms/taglist/TagList',
+		component: TagList
+	});
 </script>
 
-<script lang="ts">
-    import { Template, Story } from '@storybook/addon-svelte-csf';
-    import { fn, userEvent, within, waitFor, expect } from '@storybook/test'
-    import TagList from "./TagList.svelte";
-</script>
+<Story
+	name="Normal"
+	args={{
+		tags: ['one', 'two', 'three'],
+		clickHandler: fn()
+	}}
+	play={async ({ args, canvasElement }) => {
+		const canvas = within(canvasElement);
 
-<Template >
-    {#snippet children({ args })}
-        <TagList {...args} />
-    {/snippet}
-</Template>
+		args.tags.forEach(async (arg) => {
+			let tag = canvas.getByText(arg);
+			await userEvent.click(tag);
 
-<Story name="Normal" args={{tags: ['one', 'two', 'three'], clickHandler: fn()}} play={async ({ args, canvasElement }) => {
-    const canvas = within(canvasElement);
+			await waitFor(() => expect(args.clickHandler).toHaveBeenCalledWith(arg));
+		});
+	}}
+/>
 
-    args.tags.forEach(async (arg) => {
-
-        let tag = canvas.getByText(arg);
-        await userEvent.click(tag);
-
-        await waitFor(() => expect(args.clickHandler).toHaveBeenCalledWith(arg));
-    })
-
-}} />
-
-<Story name="Empty" args={{tags: []}} />
+<Story name="Empty" args={{ tags: [] }} />
